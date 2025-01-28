d353c89bae31d1c65ea41cfc8ccacda9300b6ee5
itznokx <dimitri.b.medeiros@gmail.com>
2025-01-28 13:05:39 -0300

    vec2.js fix and import vec2 to sketch.js

--- mfpj/local_project_ballreflection/src/vec2.js
+++ mfpj/local_project_ballreflection/src/vec2.js
@@ -1,3 +1,62 @@
-export const test = function (a) {
-  return a*a
+export default class Vec2{
+  constructor(x,y){
+    this.x = x;
+    this.y = y;
+    this.pos = null;
+    this.cor = null;
+  }
+  render (){
+    if (this.cor != null)
+      colore(this.cor[0],this.cor[1],this.cor[2])
+    if (this.pos == null)
+      seta(0,0,this.x,this.y)
+    else
+      seta(this.pos.x,
+           this.pos.y,
+           this.pos.x+this.x,
+           this.pos.y+this.y);
+  }
+  sum (v2){
+    return new Vec2(this.x+v2.x,this.y+v2.y)
+  }
+  mult (k){
+    return new Vec2(this.x*k,this.y*k);
+  }
+  // Produto Escalar (dot product)
+  dot (v2){
+    let scalar = this.x*v2.x+this.y*v2.y
+    return scalar
+  }
+  size(){
+    return sqrt(this.dot(this))
+  }
+  normalize(){
+    let invLenght = 1.0 / this.size()
+    return this.mult(invLenght);
+  }
+  cross (v2){
+    let prod = this.x*v2.y - this.y*v2.x
+    return prod;
+  }
+  dif (v2){
+    return new Vec2(this.x - v2.x,this.y - v2.y);
+  }
+  projection(n){
+    let v = new Vec2(this.x,this.y)
+    let num = v.dot(n);
+    let den = n.dot(n);
+    let div = num/den
+    let vn = n.mult(div)
+    let vp = v.dif(vn)
+    return [vn, vp]
+  }
+  reaction(n,alfa,beta){
+    let [vn,vp] = this.projection(n)
+    let rx = alfa*vp.x - beta*vn.x
+    let ry = alfa*vp.y - beta*vn.y
+    return new Vec2(rx,ry)
+  }
+  rot90(){
+    return new Vec2 (-this.y,this.x);
+  }
 }
